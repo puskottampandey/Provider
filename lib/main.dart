06ad9cbb831703e64b63be4d6a0fa1bfb9479d0b
puskottampandey <puskottam.pandey@gmail.com>
2023-07-10 17:12:08 +0545

    Consumer of provider added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:providerstatemanagement/screens/count_example.dart';
+
 
 import 'Provider/count_provider.dart';
 
